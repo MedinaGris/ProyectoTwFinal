d2b71bd6879844a263d598972414747eb23cc56f
MedinaGris <medinagris7@gmail.com>
2023-02-06 14:45:07 -0600

    Se modifica model y controller para agregar campo path al objeto cliente

--- ProyectoTwFinal/Controllers/ClientesController.cs
+++ ProyectoTwFinal/Controllers/ClientesController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProyectoTwFinal.Models;
 
@@ -56,10 +52,34 @@ namespace ProyectoTwFinal.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nombre,Direccion,Correo,Edad")] Cliente cliente)
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Direccion,Correo,Edad, File")] Cliente cliente, IFormFile File)
         {
-            if (ModelState.IsValid)
+            /*if (ModelState.IsValid)
             {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);*/
+            if (cliente != null && File != null)
+            {
+                if (File != null)
+                {
+                    string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img\\proveedor");
+                    //si es la primera imagen, crea la capeta img/provedor/
+                    if (!Directory.Exists(path))
+                        Directory.CreateDirectory(path);
+
+                    string fileName = cliente.Nombre + Path.GetFileName(File.FileName);
+                    string fileNameWithPath = Path.Combine(path, fileName);
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        //copia la imagen a su nueva direccion en wwwrot/img
+                        File.CopyTo(stream);
+                        //copia solo el nombre del archivo
+                        cliente.Path = fileName;
+                    }
+                }
                 _context.Add(cliente);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -88,12 +108,13 @@ namespace ProyectoTwFinal.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Direccion,Correo,Edad")] Cliente cliente)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Path,Nombre,Direccion,Correo,Edad")] Cliente cliente, IFormFile? FileImg)
         {
             if (id != cliente.Id)
             {
                 return NotFound();
             }
+            /*
 
             if (ModelState.IsValid)
             {
@@ -133,7 +154,52 @@ namespace ProyectoTwFinal.Controllers
                 return NotFound();
             }
 
+            return View(cliente);*/
+            // SI SE AGREGÓ NUEVA IMAGEN, SE BORRA LA ANTERIOR Y SE GUARDA LA NUEVA
+            if (FileImg != null)
+            {
+                //direccion donde se guardará la imagen
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img\\cliente");
+                //direccion de la imagen a eliminar
+                string oldpath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img\\cliente", cliente.Path);
+                //se borra la imagen anterior
+                if (System.IO.File.Exists(oldpath))
+                    System.IO.File.Delete(oldpath);
+                //crea la direccion de la imagen tomando en cuenta el nombre del usuario para no chocar nombre
+                string fileName = cliente.Nombre + Path.GetFileName(FileImg.FileName);
+                string fileNameWithPath = Path.Combine(path, fileName);
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    //copia la imagen del imput hacia la nueva direccion
+                    FileImg.CopyTo(stream);
+                    cliente.Path = fileName;
+                }
+            }
+            if (cliente != null)//Valida que el modleo no se encunetra vacio, las otras validaciones se hacen antes de entrar al metodo
+            {
+                try
+                {
+                    //actualiza la informacion de proveedor
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                //si todo sale bien regresa a la lista de provedores
+                return RedirectToAction(nameof(Index));
+            }
             return View(cliente);
+
+
         }
 
         // POST: Clientes/Delete/5
